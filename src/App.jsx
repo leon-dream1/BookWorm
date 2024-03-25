31bfd6e31805070c1959ca5563fa0e3e38baf6d9
leon-dream1~ <nahidulislam019045@gmail.com>
2024-03-26 02:16:09 +0600

    Basic project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <img src="./images//logo.svg" className='h-[100px]' alt="" />
+      {/* <h1 className="text-3xl font-bold underline font-work-sans">Vite + React</h1> */}
+    </>
+  )
+}
+
+export default App
