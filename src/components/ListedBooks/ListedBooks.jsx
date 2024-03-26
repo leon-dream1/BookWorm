4eb50589cf5d3749f67d910eab990173f8ff7a0b
leon-dream1~ <nahidulislam019045@gmail.com>
2024-03-27 03:50:56 +0600

    Sort functionality added

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import { getReadBooks, getWishListBooks } from "../../utility/storage";
 import ReadBook from "./ReadBook";
 import WishListBook from "./WishListBook";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
 
 const ListedBooks = () => {
   const booksData = useLoaderData();
@@ -30,13 +32,98 @@ const ListedBooks = () => {
     wishListBooks.push(matchingWishListBooks);
   }
 
+  const [sortBy, setSortBy] = useState("");
+
+  if (sortBy === "rating") {
+    readBooks.sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      if (a.rating < b.rating) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sortBy === "numberOfPages") {
+    readBooks.sort((a, b) => {
+      if (a.totalPages > b.totalPages) {
+        return -1;
+      }
+      if (a.totalPages < b.totalPages) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sortBy === "publisherYear") {
+    readBooks.sort((a, b) => {
+      if (a.yearOfPublishing > b.yearOfPublishing) {
+        return -1;
+      }
+      if (a.yearOfPublishing < b.yearOfPublishing) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  if (sortBy === "rating") {
+    wishListBooks.sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      if (a.rating < b.rating) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sortBy === "numberOfPages") {
+    wishListBooks.sort((a, b) => {
+      if (a.totalPages > b.totalPages) {
+        return -1;
+      }
+      if (a.totalPages < b.totalPages) {
+        return 1;
+      }
+      return 0;
+    });
+  } else if (sortBy === "publisherYear") {
+    wishListBooks.sort((a, b) => {
+      if (a.yearOfPublishing > b.yearOfPublishing) {
+        return -1;
+      }
+      if (a.yearOfPublishing < b.yearOfPublishing) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  console.log("sort", sortBy);
   return (
     <div className="container mx-auto mt-10">
       <h3 className="bg-[#1313130D] rounded-lg text-[#131313] text-[28px] font-bold font-work-sans text-center py-[30px]">
         Books
       </h3>
 
-      <div role="tablist" className="tabs tabs-lifted mt-[50px]">
+      <div className="text-center">
+        <details className="dropdown  mt-[20px]">
+          <summary className="m-1 btn">
+            Sort By <IoIosArrowDown />
+          </summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li onClick={() => setSortBy("rating")}>
+              <a>Rating</a>
+            </li>
+            <li onClick={() => setSortBy("numberOfPages")}>
+              <a>Number of pages</a>
+            </li>
+            <li onClick={() => setSortBy("publisherYear")}>
+              <a>Publisher year</a>
+            </li>
+          </ul>
+        </details>
+      </div>
+      <div role="tablist" className="tabs tabs-lifted mt-[70px]">
         <input
           type="radio"
           name="my_tabs_2"
